f96dd577bb4791f1a9900db6b4e6bf2db78b3945
Ruben Decrop <ruben@chessdevil.net>
2024-09-19 21:26:52 +0200

    added static content going live

--- frontend/components/TheSidebar.vue
+++ frontend/components/TheSidebar.vue
@@ -46,27 +46,29 @@ function rules_bjk() {
     <v-list nav class="bm-blue-grey-darken-1">
       <v-list-item to="/" :title="t('Home')" />
 
-      <!-- <v-list-group no-action>
+      <v-list-group no-action>
         <template #activator="{ props }">
           <v-list-item v-bind="props" :title="t('BYC 2025')" />
         </template>
-        <v-list-item @click="rules_bjk" :title="t('Tournament Rules')" />
+        <v-list-item to="schedule" :title="t('Game schedule')" />
+        <!-- <v-list-item @click="rules_bjk" :title="t('Tournament Rules')" />
         <v-list-item to="/trn_u8" title="U8" />
         <v-list-item to="/trn_u10" title="U10" />
         <v-list-item to="/trn_u12" title="U12" />
         <v-list-item to="/trn_u14" title="U14" />
         <v-list-item to="/trn_u16" title="U16" />
         <v-list-item to="/trn_u18" title="U18" />
-        <v-list-item to="/trn_u20" title="U20" />
+        <v-list-item to="/trn_u20" title="U20" /> -->
       </v-list-group>
       <v-list-group no-action>
         <template #activator="{ props }">
           <v-list-item v-bind="props" title="Info" />
         </template>
-        <v-list-item @click="stay" :title="t('Stay')" />
-
-        <v-list-item @click="livegames_" title="Live" />
-      </v-list-group> -->
+        <v-list-item to="/stay" :title="t('stay.stay')" />
+        <v-list-item to="/catering" :title="t('stay.catering')" />
+        <v-list-item to="/reservation" :title="t('stay.reservation')" />
+        <!-- <v-list-item @click="livegames_" title="Live" /> -->
+      </v-list-group>
     </v-list>
   </div>
 </template>
