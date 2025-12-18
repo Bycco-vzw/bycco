49a5db0088afb52b8ae44a04f352ac3421003a3a
Ruben Decrop <ruben@chessdevil.net>
2025-12-18 22:14:16 +0100

    sponsor update

--- frontend/components/AdCarousel.vue
+++ frontend/components/AdCarousel.vue
@@ -5,19 +5,24 @@ const current = ref(0)
 const interval = 6
 const items = [
   { src: "/img/frame_archery.png", name: "archery" },
+  { src: "/img/frame_chesscom.png", name: "chesscom" },
   { src: "/img/frame_chessconsult.png", name: "chessconsult" },
+  { src: "/img/frame_chessnut.png", name: "chessnut" },
+  { src: "/img/frame_google.png", name: "google" },
   { src: "/img/frame_talistro.png", name: "talistro" },
-  { src: "/img/frame_chronos.png", name: "chronos" },
-  { src: "/img/frame_cvwarehouse.png", name: "cvwarehouse" },
   { src: "/img/frame_dgt.png", name: "dgt" },
+  { src: "/img/frame_newinchess.png", name: "newinchess" },
+  { src: "/img/frame_printcity.png", name: "printcity" },
+  { src: "/img/frame_printedchess.png", name: "printedchess" },
   { src: "/img/frame_floreal.png", name: "floreal" },
-  { src: "/img/frame_standaardboekhandel.png", name: "standaardboekhandel" },
   { src: "/img/frame_thinkerspublishing.png", name: "thinkerspublishing" },
 ]
 
 function setupCarousel() {
   let secCarousel = Math.floor((new Date() / 1000) % (interval * items.length))
+  console.log("secCarousel", secCarousel)
   current.value = Math.floor(secCarousel / items.length)
+  console.log("current", current.value)
   if (current.value == items.length) current.value = 0
 }
 
@@ -29,7 +34,7 @@ onMounted(() => setupCarousel())
     <v-carousel
       :show-arrows="false"
       hide-delimiters
-      :value="current"
+      :model-value="current"
       height="170"
       class="adcarousel mb-2"
       :interval="interval * 1000"
