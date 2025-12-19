6126df1a74c4bc8c9d614a128a33aef9298ede35
Ruben Decrop <ruben@chessdevil.net>
2025-12-19 21:24:49 +0100

    payment request 2026

--- frontend/api/payment.js
+++ frontend/api/payment.js
@@ -33,12 +33,10 @@ export default {
       },
     })
   },
-
-  // particpant vk
-  mgmt_create_participant_vk_pr: async function (options) {
+  mgmt_create_participant_pr: async function (options) {
     const { token, id } = options
     return await axios.post(
-      `${prefix}/participant_vk_pr/${id}`,
+      `${prefix}/participant_pr/${id}`,
       {},
       {
         headers: {
@@ -47,10 +45,10 @@ export default {
       }
     )
   },
-  mgmt_create_participants_vk_pr: async function (options) {
+  mgmt_create_participants_pr: async function (options) {
     const { token } = options
     return await axios.post(
-      `${prefix}/participant_vk_pr`,
+      `${prefix}/participant__pr`,
       {},
       {
         headers: {
@@ -59,67 +57,23 @@ export default {
       }
     )
   },
-  mgmt_delete_participant_vk_pr: async function (options) {
+  mgmt_delete_participant_pr: async function (options) {
     const { token, id } = options
-    return await axios.delete(`${prefix}/participant_vk_pr/${id}`, {
+    return await axios.delete(`${prefix}/participant_pr/${id}`, {
       headers: {
         Authorization: "Bearer " + token,
       },
     })
   },
-  mgmt_update_participant_vk_pr: async function (options) {
-    const { token, id, prq } = options
-    return await axios.put(`${prefix}/participant_vk_pr/${id}`, prq, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-  },
-
-  // particpant bjk
-  mgmt_create_participant_bjk_pr: async function (options) {
-    const { token, id } = options
-    return await axios.post(
-      `${prefix}/participant_bjk_pr/${id}`,
-      {},
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-  },
-  mgmt_create_participants_bjk_pr: async function (options) {
-    const { token } = options
-    return await axios.post(
-      `${prefix}/participant_bjk_pr`,
-      {},
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    )
-  },
-  mgmt_delete_participant_bjk_pr: async function (options) {
-    const { token, id } = options
-    return await axios.delete(`${prefix}/participant_bjk_pr/${id}`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-  },
-  mgmt_update_participant_bjk_pr: async function (options) {
+  mgmt_update_participant_pr: async function (options) {
     console.log("calling api update pr bjk", options)
     const { token, id, prq } = options
-    return await axios.put(`${prefix}/participant_bjk_pr/${id}`, prq, {
+    return await axios.put(`${prefix}/participant_pr/${id}`, prq, {
       headers: {
         Authorization: "Bearer " + token,
       },
     })
   },
-
-  // general
   mgmt_email_pr: async function (options) {
     const { token, id, prq } = options
     return await axios.post(`${prefix}/email_pr/${id}`, prq, {
